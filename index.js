8150daf88bb0a53b1f384df9d64fca4c89bc549f
Samira <samiraak@code.edu.az>
2023-10-22 17:15:32 +0400

    cmmt

---
+++ index.js
@@ -0,0 +1,40 @@
+//Task 1
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 
+};
+for (const [key,value] of Object.entries(student)) {
+    console.log(key);
+}
+//Task 2
+delete student.rollno
+for (const key of Object.entries(student)) {
+    console.log(key);
+}
+
+//Task 3
+const objSum = {
+    a: 10,
+    b: 10,
+};
+const updatedObjSum = {
+    ...objSum,
+    sum: objSum.a + objSum.b
+  };
+  
+  console.log(updatedObjSum);
+
+//Task 4
+const obj = {
+    n: 13,
+    info: "n is smaller than 20"
+  };
+  
+  const updatedObj = { ...obj };
+  
+  if (obj.n > 10) {
+    updatedObj.info = "n is bigger than 10";
+  }
+  
+  console.log(updatedObj);
